55a5a7bc5fd5cdb6de6e721f5cc0871ff5864681
Tressa Killilea <t.killilea@snhu.edu>
2023-08-28 15:55:09 -0400

    add slider - broken

--- color-game/src/components/slider/index.js
+++ color-game/src/components/slider/index.js
@@ -1,10 +1,16 @@
 import ReactSlider from "react-slider";
+import "./style.css";
+
 
 function Slider(props){
     return(
         <div className="slider_wrapper">
             <div className="color_name">{props.color}</div>
-            <ReactSlider></ReactSlider>
+            <ReactSlider
+                className="cust_slider"
+                trackClassName="cust_slider-track"
+                thumbClassName="cust_slider-thumb"
+                />
         </div>
     );
 }
