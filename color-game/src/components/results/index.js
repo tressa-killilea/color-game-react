26c3c65d63580fea18904ef4c41033d94ecc05a7
Tressa Killilea <t.killilea@snhu.edu>
2023-08-29 14:09:45 -0400

    style results

--- color-game/src/components/results/index.js
+++ color-game/src/components/results/index.js
@@ -14,22 +14,62 @@ class Result extends React.Component{
         if(perc < 0) perc = perc * -1;
         perc = Math.round(perc);
         return 100 - perc;
-        
+    }
+
+    getGradient(){
+        var perc = this.getPercentage();
+        var colors;
+        if(perc > 80){
+            colors = {
+                inner: "#56bf56",
+                outer: "#017801"
+            }
+        } else if(perc > 60){
+            colors = {
+                inner: "#dfe079",
+                outer: "#bfc21f"
+            }
+        } else{
+            colors = {
+                inner: "#d9685b",
+                outer: "#c91f0c"
+            }
+        }
+        return `radial-gradient(${colors.inner}, ${colors.outer})`;
     }
 
     render(){
         return(
-            <div>
+            <div className="results_wrapper">
                 <div>
-                    <p>{`Your Guess: ${this.props.guess}`}</p>
-                    <p>{`Answer: ${this.props.goal}`}</p>
+                    <table>
+                       <tbody>
+                       <tr>
+                            <th>Your Guess</th>
+                            <th>Answer</th>
+                        </tr>
+                        <tr>
+                            <td className="score red">{this.props.guess[0]}</td>
+                            <td className="score red">{this.props.goal[0]}</td>
+                        </tr>
+                        <tr>
+                            <td className="score green">{this.props.guess[1]}</td>
+                            <td className="score green">{this.props.goal[1]}</td>
+                        </tr>
+                        <tr>
+                            <td className="score blue">{this.props.guess[2]}</td>
+                            <td className="score blue">{this.props.goal[2]}</td>
+                        </tr>
+                       </tbody>
+                    </table>
                 </div>
-                <div>
-                    <p className="percentage">{`${this.getPercentage()}%`}</p>
+                <div className="percentage" style={{background: this.getGradient()}}>
+                    <p>{`${this.getPercentage()}%`}</p>
                 </div>
             </div>
         );
     }
 }
+//    background: radial-gradient(#56bf56, #017801);
 
 export default Result;
